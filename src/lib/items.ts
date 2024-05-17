1a493defb1daf72b8f9a78a0c38fb0a9a57e9d8e
giovannivicentin <giovannifvicentin@gmail.com>
2024-05-17 14:18:20 -0300

    feat: Update Home page grid with new items

---
+++ src/lib/items.ts
@@ -0,0 +1,42 @@
+export const items = [
+  {
+    name: 'Elementos da Tabela Periódica',
+    pageToGo: '/periodic-table',
+  },
+  {
+    name: 'Relógio de Sol',
+    pageToGo: '/sundial',
+  },
+  {
+    name: 'Circuito Elétrico',
+    pageToGo: '/electric-circuit',
+  },
+  {
+    name: 'Catapulta',
+    pageToGo: '/catapult',
+  },
+  {
+    name: 'Quebra Cabeça',
+    pageToGo: '/puzzle',
+  },
+  {
+    name: 'Quadro Branco',
+    pageToGo: '/whiteboard',
+  },
+  {
+    name: 'Xadrez',
+    pageToGo: '/chess',
+  },
+  {
+    name: 'Damas',
+    pageToGo: '/checkers',
+  },
+  {
+    name: 'Jogo da Velha',
+    pageToGo: '/tic-tac-toe',
+  },
+  {
+    name: 'Spinner de Matemática',
+    pageToGo: '/math-spinner',
+  },
+]
