39bc96e2628b5df0edc4e1c867fb380fab701d08
giovannivicentin <giovannifvicentin@gmail.com>
2024-05-17 13:54:17 -0300

    feat: Add new pages for Catapult, Checkers, Chess, Electric Circuit, Math Spinner, Periodic Table, Puzzle, Sundial, Tic Tac Toe, and Whiteboard

---
+++ src/app/math-spinner/page.tsx
@@ -0,0 +1,9 @@
+function MathSpinnerPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center mx-4 md:mx-0">
+      HELLO WORLD
+    </div>
+  )
+}
+
+export default MathSpinnerPage
