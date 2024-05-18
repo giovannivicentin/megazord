ac963ff0c5677a955543d89cb45fa43a7c8cc1f4
giovannivicentin <giovannifvicentin@gmail.com>
2024-05-18 17:46:14 -0300

    refactor: Update ChessPage to use ChessGame component

--- src/app/chess/page.tsx
+++ src/app/chess/page.tsx
@@ -1,7 +1,9 @@
+import ChessGame from '@/components/chess-game'
+
 function ChessPage() {
   return (
     <div className="flex min-h-screen flex-col items-center mx-4 md:mx-0">
-      HELLO WORLD
+      <ChessGame />
     </div>
   )
 }
