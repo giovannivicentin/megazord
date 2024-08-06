539718371547c77312a65b7e41d5feb58f586e2d
felipedestroo <fdestrocontact@gmail.com>
2024-08-06 16:53:05 -0300

    fix: title

--- src/app/whiteboard/page.tsx
+++ src/app/whiteboard/page.tsx
@@ -3,7 +3,7 @@ import WhiteboardCanvas from '@/components/whiteboard'
 function WhiteboardPage() {
   return (
     <div className="flex min-h-screen flex-col items-center mx-4 md:mx-0">
-      <h1 className="text-4xl font-bold mt-8 mb-8">Whiteboard Drawing</h1>
+      <h1 className="text-4xl font-bold mt-8 mb-8">Quadro Branco</h1>
       <WhiteboardCanvas />
     </div>
   )
