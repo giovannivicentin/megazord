59e488aa3dd8d25baef2c1379cb38dd847d5aaef
felipedestroo <fdestrocontact@gmail.com>
2024-08-11 12:04:12 -0300

    feat: create periodic table game

--- src/app/periodic-table/page.tsx
+++ src/app/periodic-table/page.tsx
@@ -1,7 +1,9 @@
+import PeriodicTable from '@/components/periodic-table'
+
 function PeriodicTablePage() {
   return (
     <div className="flex min-h-screen flex-col items-center mx-4 md:mx-0">
-      HELLO WORLD
+      <PeriodicTable />
     </div>
   )
 }
---
+++ src/components/periodic-table.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+const generateDeck = () => {
+  const memoryCards = [
+    'aluminium',
+    'fluorine',
+    'helium',
+    'mercury',
+    'plutonium',
+    'potassium',
+    'radium',
+    'sulfur',
+  ]
+
+  const deck = [...memoryCards, ...memoryCards]
+  return deck.sort(() => Math.random() - 0.5)
+}
+
+export default function PeriodicTable() {
+  const [cards, setCards] = useState<string[]>(generateDeck())
+  const [flipped, setFlipped] = useState<number[]>([])
+  const [solved, setSolved] = useState<number[]>([])
+
+  useEffect(() => {
+    const checkForMatch = () => {
+      const [first, second] = flipped
+
+      if (cards[first] === cards[second]) {
+        setSolved([...solved, ...flipped])
+      }
+      setFlipped([])
+    }
+
+    if (flipped.length === 2) {
+      setTimeout(() => {
+        checkForMatch()
+      }, 1000)
+    }
+  }, [cards, flipped, solved])
+
+  const handleClick = (index: number) => {
+    if (!flipped.includes(index) && flipped.length < 2) {
+      setFlipped([...flipped, index])
+    }
+  }
+
+  const gameOver = solved.length === cards.length
+
+  const resetGame = () => {
+    setCards(generateDeck())
+    setFlipped([])
+    setSolved([])
+  }
+
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl mt-12 mb-12">Elementos da Tabela Periódica</h1>
+      {gameOver && <h2 className="p-5 text-3xl">Você Venceu!</h2>}
+      <div className="grid grid-cols-4 gap-5 mt-5">
+        {cards.map((card, index) => (
+          <div
+            className={`flex justify-center text-4xl font-bold text-black items-center rounded-lg w-36 bg-primary h-36 transform cursor-pointer transition-transform duration-300 ${flipped.includes(index) || solved.includes(index) ? 'rotate-180' : ''}`}
+            key={index}
+            onClick={() => handleClick(index)}
+          >
+            {flipped.includes(index) || solved.includes(index) ? (
+              <Image
+                className="rotate-180 rounded-lg"
+                src={`/memory-cards/${card}.png`}
+                fill
+                alt="Memory Card"
+              />
+            ) : (
+              '?'
+            )}
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={resetGame}
+        className="w-64 h-14 lg:h-20 text-white border-none outline-none cursor-pointer rounded-lg bg-primary text-xl lg:text-2xl mt-10 mb-8 hover:bg-primary/90"
+      >
+        Reiniciar
+      </button>
+    </div>
+  )
+}
