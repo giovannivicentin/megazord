a38c7f1f873408f2cb8765189ace7f2f3de8b71b
felipedestroo <fdestrocontact@gmail.com>
2024-09-16 21:34:27 -0300

    feat: link puzzle component

--- src/app/puzzle/page.tsx
+++ src/app/puzzle/page.tsx
@@ -1,7 +1,9 @@
+import { PuzzleGame } from '@/components/puzzle'
+
 function PuzzlePage() {
   return (
     <div className="flex min-h-screen flex-col items-center mx-4 md:mx-0">
-      HELLO WORLD
+      <PuzzleGame />
     </div>
   )
 }
