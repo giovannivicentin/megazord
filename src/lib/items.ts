15499f08efeec4bea29bcd10e2b6c7bec7732667
giovannivicentin <giovannifvicentin@gmail.com>
2025-01-28 16:11:15 -0300

    feat: add imageUrl for various items and remove unused components

--- src/lib/items.ts
+++ src/lib/items.ts
@@ -2,41 +2,41 @@ export const items = [
   {
     name: 'Elementos da Tabela Periódica',
     pageToGo: '/periodic-table',
+    imageUrl: '/megaHelmetWhite.svg',
   },
   {
     name: 'Relógio de Sol',
     pageToGo: '/sundial',
-  },
-  {
-    name: 'Circuito Elétrico',
-    pageToGo: '/electric-circuit',
-  },
-  {
-    name: 'Catapulta',
-    pageToGo: '/catapult',
+    imageUrl: '/megaHelmetWhite.svg',
   },
   {
     name: 'Quebra Cabeça',
     pageToGo: '/puzzle',
+    imageUrl: '/megaHelmetWhite.svg',
   },
   {
     name: 'Quadro Branco',
     pageToGo: '/whiteboard',
+    imageUrl: '/megaHelmetWhite.svg',
   },
   {
     name: 'Xadrez',
     pageToGo: '/chess',
+    imageUrl: '/megaHelmetWhite.svg',
   },
   {
     name: 'Damas',
     pageToGo: '/checkers',
+    imageUrl: '/megaHelmetWhite.svg',
   },
   {
     name: 'Jogo da Velha',
     pageToGo: '/tic-tac-toe',
+    imageUrl: '/megaHelmetWhite.svg',
   },
   {
     name: 'Spinner de Matemática',
     pageToGo: '/math-spinner',
+    imageUrl: '/megaHelmetWhite.svg',
   },
 ]
