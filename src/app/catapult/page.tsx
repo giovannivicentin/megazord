f5f088bf7e9cbd3bb45d2b95d31db000b717477a
giovannivicentin <giovannifvicentin@gmail.com>
2025-01-28 17:11:36 -0300

    feat: remove CatapultPage and ElectricCircuitPage components

--- src/app/catapult/page.tsx
+++
@@ -1,9 +0,0 @@
-function CatapultPage() {
-  return (
-    <div className="flex min-h-screen flex-col items-center mx-4 md:mx-0">
-      HELLO WORLD
-    </div>
-  )
-}
-
-export default CatapultPage
