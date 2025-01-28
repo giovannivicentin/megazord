731e2ce3c512f96bde6c1c3ae4079f64e250da10
giovannivicentin <giovannifvicentin@gmail.com>
2025-01-28 17:09:18 -0300

    feat: add image URLs for various game items

--- src/lib/items.ts
+++ src/lib/items.ts
@@ -2,41 +2,51 @@ export const items = [
   {
     name: 'Elementos da Tabela Periódica',
     pageToGo: '/periodic-table',
+    imageUrl: '/preview-pages/chess.png',
   },
   {
     name: 'Relógio de Sol',
     pageToGo: '/sundial',
+    imageUrl: '/preview-pages/chess.png',
   },
   {
     name: 'Circuito Elétrico',
     pageToGo: '/electric-circuit',
+    imageUrl: '/preview-pages/chess.png',
   },
   {
     name: 'Catapulta',
     pageToGo: '/catapult',
+    imageUrl: '/preview-pages/chess.png',
   },
   {
     name: 'Quebra Cabeça',
     pageToGo: '/puzzle',
+    imageUrl: '/preview-pages/puzzle.png',
   },
   {
     name: 'Quadro Branco',
     pageToGo: '/whiteboard',
+    imageUrl: '/preview-pages/chess.png',
   },
   {
     name: 'Xadrez',
     pageToGo: '/chess',
+    imageUrl: '/preview-pages/chess.png',
   },
   {
     name: 'Damas',
     pageToGo: '/checkers',
+    imageUrl: '/preview-pages/chess.png',
   },
   {
     name: 'Jogo da Velha',
     pageToGo: '/tic-tac-toe',
+    imageUrl: '/preview-pages/chess.png',
   },
   {
     name: 'Spinner de Matemática',
     pageToGo: '/math-spinner',
+    imageUrl: '/preview-pages/chess.png',
   },
 ]
