8215d3631b92bf6bbab2f948f73f460bdc3b526a
giovannivicentin <giovannifvicentin@gmail.com>
2025-02-11 17:31:02 -0300

    fix: adjust layout and spacing in TicTacToePage component

--- src/app/tic-tac-toe/page.tsx
+++ src/app/tic-tac-toe/page.tsx
@@ -3,8 +3,8 @@ import TicTacToe from '@/components/tic-tac-toe'
 function TicTacToePage() {
   return (
     <>
-      <div className="min-h-screen flex flex-col items-center justify-center p-4 bg-neutral-50 dark:bg-[unset]">
-        <h1 className="text-3xl font-bold mb-6">Jogo da Velha</h1>
+      <div className="flex min-h-screen flex-col items-center px-4 pt-8 dark:bg-[unset]">
+        <h1 className="text-3xl font-bold mb-28">Jogo da Velha</h1>
         <TicTacToe />
       </div>
     </>
