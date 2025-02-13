b3536820494b1a1a76c6532bfb7da72f8bb2db43
giovannivicentin <giovannifvicentin@gmail.com>
2025-02-13 12:13:08 -0300

    feat: enhance MoveHistory component to handle empty moves array and display message

--- src/components/checkers/move-history.tsx
+++ src/components/checkers/move-history.tsx
@@ -1,18 +1,24 @@
 interface MoveHistoryProps {
-  moves: string[]
+  moves?: string[]
 }
 
-export function MoveHistory({ moves }: MoveHistoryProps) {
+export function MoveHistory({ moves = [] }: MoveHistoryProps) {
   return (
     <div className="bg-white dark:bg-background p-4 rounded-lg max-h-[50vh] shadow-md border border-muted lg:max-h-[80vh] overflow-scroll">
       <h2 className="text-xl font-bold mb-2">Histórico de Jogadas</h2>
-      <ul className="list-decimal list-inside">
-        {moves.map((move, index) => (
-          <li key={index} className="mb-1">
-            {move}
-          </li>
-        ))}
-      </ul>
+      {Array.isArray(moves) && moves.length > 0 ? (
+        <ul className="list-decimal list-inside">
+          {moves.map((move, index) => (
+            <li key={index} className="mb-1">
+              {move}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-sm text-muted-foreground">
+          Nenhuma jogada realizada ainda.
+        </p>
+      )}
     </div>
   )
 }
